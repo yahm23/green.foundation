f267c250e9a744eb759505eab993119db61968fd
Yousef Ahmed <yahmed1996a@gmail.com>
2020-04-28 10:41:29 +0100

    Formatted a bit, added some colour

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ function App() {
   return (
     <Router>
       <nav>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
-          <li><Link to='/page1'>Page 1</Link></li>
-          <li><Link to='/page2'>Page 2</Link></li>
-          <li><Link to='/contact'>Contact</Link></li>
+        <ul className="navbar">
+          <h3><Link to='/'>Home</Link></h3>
+          <h3><Link to='/about'>About</Link></h3>
+          <h3><Link to='/page1'>Page 1</Link></h3>
+          <h3><Link to='/page2'>Page 2</Link></h3>
+          <h3><Link to='/contact'>Contact</Link></h3>
         </ul>
       </nav>
       <Switch>
