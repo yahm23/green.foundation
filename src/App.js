a78932e722cc85f05a88c036c2e000a1c5f677a4
Yousef Ahmed <yahmed1996a@gmail.com>
2020-04-29 13:44:18 +0100

    needed commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
-
-import Home from './pages/home.js';
-import About from './pages/about.js';
-import Contact from './pages/contact.js';
-import Page1 from './pages/page1.js';
-import Page2 from './pages/page2.js';
-// import { render } from '@testing-library/react';
 
 function App() {
   return (
+<<<<<<< HEAD
     <Router>
       <nav>
         <ul className="navbar">
@@ -30,25 +24,25 @@ function App() {
         <Route render={()=><h1> 404: Page not found</h1>}/>
       </Switch>
     </Router>
+=======
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+>>>>>>> parent of b3b5d03... Basic Routing complete
   );
 }
-
-// const home = ()=>{
-//   return(){render}
-//   <Home></Home>
-// }
-// const about = ()=>{
-//   <About></About>
-// }
-// const contact = ()=>{
-//   <Contact></Contact>
-// }
-// const page1 = ()=>{
-//   <Page1></Page1>
-// }
-
-// const page2 = ()=>{
-//   <Page2></Page2>
-// }
 
 export default App;
